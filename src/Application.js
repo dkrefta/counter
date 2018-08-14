8346bb9bf688be94eb3e38d41a64351d4d04ef41
Diego Krefta <diego.krefta@gmail.com>
2018-08-14 12:53:06 -0300

    State Count

--- src/Application.js
+++ src/Application.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 import Counter from './Counter';
-// import CounterContainer from './CounterContainer';
-// import WithCount from './WithCount';
+import CounterContainer from './CounterContainer';
+import WithCount from './WithCount';
 
 export default class Application extends Component {
   render() {
--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
 
+
 export default class Counter extends Component {
+
+ state = {
+   count: 0,
+ }
+ 
+onClickInc (value) {
+  const {count} = this.state
+      this.setState({
+        count: count +1
+      })
+  }
+onClickDec (value) {
+  const {count} = this.state
+      this.setState({
+        count: count -1
+      })
+  }
+onClickReset (value) {
+  const {count} = this.state
+      this.setState({
+        count: 0
+      })
+  }
+
+ 
   render() {
+
+    const {count} = this.state
     return (
       <section className="Counter">
-        <h1>Count: {0}</h1>
-        <button onClick={() => {}} className="full-width">Increment</button>
-        <button onClick={() => {}} className="full-width">Decrement</button>
-        <button onClick={() => {}} className="full-width">Reset</button>
+        <h1>Count: {this.state.count}</h1>
+        <button onClick={(e) => this.onClickInc(this, 'add')} value="add"className="full-width">Increment</button>
+        <button onClick={(e) => this.onClickDec(this, 'sub')} className="full-width">Decrement</button>
+        <button onClick={(e) => this.onClickReset(this, 'reset')} className="full-width">Reset</button>
       </section>
     );
   }
